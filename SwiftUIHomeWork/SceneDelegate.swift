c420dc7b7528beaef01e3cbbbf7481ae1bd664f5
tooszovski <tooszovsky@gmail.com>
2019-12-07 19:50:43 +0300

    Initial commit

---
+++ SwiftUIHomeWork/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  SwiftUIHomeWork
+//
+//  Created by Aleksander Tooszovsky on 07.12.2019.
+//  Copyright © 2019 NA. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+  var window: UIWindow?
+
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    let contentView = ContentView()
+
+    // Use a UIHostingController as window root view controller.
+    if let windowScene = scene as? UIWindowScene {
+      let window = UIWindow(windowScene: windowScene)
+      window.rootViewController = UIHostingController(rootView: contentView)
+      self.window = window
+      window.makeKeyAndVisible()
+    }
+  }
+}
+
