8ef76b71dcee6fd09fa50643e05a0045784c501c
tooszovski <tooszovsky@gmail.com>
2019-12-08 15:36:02 +0300

    Add Detail View

---
+++ SwiftUIHomeWork/MakerView.swift
@@ -0,0 +1,22 @@
+//
+//  MakerView.swift
+//  SwiftUIHomeWork
+//
+//  Created by Aleksander Tooszovsky on 08.12.2019.
+//  Copyright © 2019 NA. All rights reserved.
+//
+
+import SwiftUI
+
+struct MakerView: View {
+  var body: some View {
+    Text("Car Maker")
+      .fontWeight(.heavy)
+  }
+}
+
+struct MakerView_Previews: PreviewProvider {
+  static var previews: some View {
+    MakerView()
+  }
+}
